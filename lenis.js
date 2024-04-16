5ad33fea42bb8ffc6c452f1669705a84c177d4d2
오하늘 <>
2024-04-16 16:43:11 +0900

    opacity

--- lenis.js
+++ lenis.js
@@ -12,24 +12,26 @@ function updateTransformToScroll(prev, next, scroll, start, end) {
   return value;
 }
 
-const images = document.querySelectorAll("#parallax img");
+const images = document.querySelectorAll("#parallax .img-box");
 
 function scaleToScroll(top, initValue, targetValue) {
   const threshold = window.innerHeight;
+
   if (top < threshold) {
     const value = Math.max(0, top) / threshold;
     return initValue + (targetValue - initValue) * value;
   }
 
-  return initValue;
+  return targetValue;
 }
 
 lenis.on("scroll", (scroll) => {
   images.forEach((image) => {
+    const img = image.querySelector("img");
     const { top } = image.getBoundingClientRect();
     const scale = scaleToScroll(top, 1, 1.6);
     const opacity = scaleToScroll(top, 1, 0);
-    image.style.transform = `scale(${scale})`;
-    image.style.opacity = opacity;
+    img.style.transform = `scale(${scale})`;
+    img.style.opacity = opacity;
   });
 });
