3fe11f0d887e9387050e3890bfbf20dc7a12a32b
오하늘 <nebulass@naver.com>
2024-04-16 18:24:01 +0900

    hover action, opacity 수치 조절

--- lenis.js
+++ lenis.js
@@ -13,24 +13,41 @@ function updateTransformToScroll(prev, next, scroll, start, end) {
 }
 
 const images = document.querySelectorAll("#parallax .img-box");
+const valueText = document.querySelector("#parallax .value");
+// console.log(valueText);
 
-function scaleToScroll(top, initValue, targetValue) {
-  const threshold = window.innerHeight;
+// const checkPoint = document.createElement("div");
+// checkPoint.innerHTML = "_______END";
+// checkPoint.style.position = "fixed";
+// checkPoint.style.top = "10%";
+// checkPoint.style.color = "red";
+// checkPoint.style.fontSize = "32px";
+// checkPoint.style.zIndex = 1000;
+// document.body.appendChild(checkPoint);
 
-  if (top < threshold) {
-    const value = Math.max(0, top) / threshold;
-    return initValue + (targetValue - initValue) * value;
+function scaleToScroll(top, prev, next, endPoint) {
+  let height = window.innerHeight;
+  let value;
+  if (top < height) {
+    if (endPoint) {
+      value = Math.max(0, top) / (height - (1 - endPoint)) - (1 - endPoint);
+      // valueText.innerHTML = `${value}`;
+      if (value < 0) return next;
+      return next + (prev - next) * value;
+    }
+    value = Math.max(0, top) / height;
+
+    return next + (prev - next) * value;
   }
-
-  return targetValue;
+  return prev;
 }
 
 lenis.on("scroll", (scroll) => {
   images.forEach((image) => {
     const img = image.querySelector("img");
     const { top } = image.getBoundingClientRect();
-    const scale = scaleToScroll(top, 1, 1.6);
-    const opacity = scaleToScroll(top, 1, 0);
+    const scale = scaleToScroll(top, 1.6, 1, 0.8);
+    const opacity = scaleToScroll(top, 0, 1, 0.8);
     img.style.transform = `scale(${scale})`;
     img.style.opacity = opacity;
   });
