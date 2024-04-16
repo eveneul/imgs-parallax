f603b82d487f2a032338e82a6dbe114595ad506d
오하늘 <>
2024-04-16 16:37:44 +0900

    수치 조절

--- lenis.js
+++ lenis.js
@@ -20,6 +20,7 @@ function scaleToScroll(top, initValue, targetValue) {
     const value = Math.max(0, top) / threshold;
     return initValue + (targetValue - initValue) * value;
   }
+
   return initValue;
 }
 
@@ -27,7 +28,7 @@ lenis.on("scroll", (scroll) => {
   images.forEach((image) => {
     const { top } = image.getBoundingClientRect();
     const scale = scaleToScroll(top, 1, 1.6);
-    const opacity = scaleToScroll(top, 1, 0.5);
+    const opacity = scaleToScroll(top, 1, 0);
     image.style.transform = `scale(${scale})`;
     image.style.opacity = opacity;
   });
